5ed7fdccb2e6c19e4defaf9f62ec5be1ee236b86
Pika <pika@lasagna.dev>
2020-08-07 11:07:16 -0400

    reformat with black

--- bot.py
+++ bot.py
@@ -1,20 +1,20 @@
 import discord
 from discord.ext import commands
-import random,os
+import random, os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 # client = discord.Client()
-bot = commands.Bot(command_prefix='$')
-TOKEN = os.environ.get('TOKEN')
-
+bot = commands.Bot(command_prefix="$")
+TOKEN = os.environ.get("TOKEN")
 
 
 # Registering an event
 @bot.event
 async def on_ready():
-    print('Logged in as')
+    print("Logged in as")
     print(bot.user.name)
     print(bot.user.id)
 
@@ -23,12 +23,12 @@ async def on_ready():
 async def roll(ctx, dice: str):
     # Rolls a dice in NdN format.
     try:
-        rolls, limit = map(int, dice.split('d'))
+        rolls, limit = map(int, dice.split("d"))
     except Exception:
-        await ctx.send('Format has to be in NdN!')
+        await ctx.send("Format has to be in NdN!")
         return
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    result = ", ".join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
 
 
