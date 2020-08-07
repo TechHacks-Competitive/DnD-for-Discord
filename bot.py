7471c5eda83e24561f468b2742dcab4ee67af21c
Pika <pika@lasagna.dev>
2020-08-07 11:34:40 -0400

    periodic status updating, default dice roll

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import random
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,27 +12,34 @@ bot = commands.Bot(command_prefix="$")
 TOKEN = os.environ.get("TOKEN")
 
 
-@bot.event
-async def on_ready():
-    print("Logged in as")
-    print(bot.user.name)
-    servers = list(bot.guilds)
-    server_num = len(servers)
-    print(bot.user.id)
+async def update_status():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name=f"over {server_num} servers || $help",
+            name=f"over {len(bot.guilds)} servers || $help",
         )
     )
 
 
+@bot.event
+async def on_ready():
+    print("Logged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    await update_status()
+
+
+@tasks.loop(minutes=10)
+async def update_status_loop():
+    await update_status()
+
+
 @bot.command()
-async def roll(ctx, dice: str):
+async def roll(ctx, dice: str = "1d20"):
     """Rolls a dice in NdN format."""
     try:
         rolls, limit = map(int, dice.split("d"))
-    except Exception:
+    except:
         await ctx.send("Format has to be in NdN!")
         return
 
