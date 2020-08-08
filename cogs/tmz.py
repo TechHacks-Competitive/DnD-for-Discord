7045575f5e0858f086fecc759a415e2309a2f1fe
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-08-07 20:28:57 -0400

    tmz.py

---
+++ cogs/tmz.py
@@ -0,0 +1,30 @@
+# tmz.py
+import discord, time, random
+from discord.ext import commands
+from discord import Member
+
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+from pytz import timezone, utc
+
+class TMZ(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(name="get_offset")
+    async def get_offset(self,ctx, lat,lng):
+        """
+        returns a location's time zone offset from UTC in minutes.
+        """
+
+        today = datetime.now()
+        tf = TimezoneFinder()
+        tz_target = timezone(tf.certain_timezone_at(lng=lng, lat=lat))
+        today_target = tz_target.localize(today)
+        today_utc = utc.localize(today)
+        await ctx.send( (today_utc - today_target).total_seconds() / 60 )
+
+
+
+def setup(bot):
+    bot.add_cog(TMZ(bot))
