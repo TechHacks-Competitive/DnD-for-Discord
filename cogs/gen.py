f9a3e6a6ebda95381d3158637ece037d4502bb9c
Pika <pika@lasagna.dev>
2020-08-08 12:19:12 -0400

    latlong

--- cogs/gen.py
+++ cogs/gen.py
@@ -4,13 +4,13 @@ from discord.ext import commands
 from discord import Member
 import json
 
-with open('loot.json') as f:
-    data = json.load(f)  
-    
+with open("loot.json") as f:
+    data = json.load(f)
+
 weighted_tb = []
 
-class Gen(commands.Cog):    
-          
+
+class Gen(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -18,20 +18,20 @@ class Gen(commands.Cog):
     async def on_ready(self):
         print("gen.py is active")
         loot_table = data["level_1"]["magic_tb"]
-        for item,weight in loot_table:
+        for item, weight in loot_table:
             for _ in range(weight):
                 weighted_tb.append(item)
 
     @commands.command()
     async def hi(self, ctx):
         await ctx.send("this was sent from a cog lol")
-         
+
     @commands.command()
     async def loot(self, ctx, items=1):
         invn = ""
         for _ in range(items):
-            invn += (random.choice(weighted_tb)) + '\n'
-        await ctx.send('```'+invn+'```')
+            invn += (random.choice(weighted_tb)) + "\n"
+        await ctx.send("```" + invn + "```")
 
 
 def setup(bot):
--- cogs/tmz.py
+++ cogs/tmz.py
@@ -1,13 +1,16 @@
 # tmz.py
-import discord, time, random
-from discord.ext import commands
-from discord import Member
 import json
-
-from timezonefinder import TimezoneFinder
+import os
+import random
+import time
 from datetime import datetime
-from pytz import timezone, utc, country_timezones
+
 import aiohttp
+import discord
+from discord import Member
+from discord.ext import commands
+from pytz import country_timezones, timezone, utc
+from timezonefinder import TimezoneFinder
 
 
 class TMZ(commands.Cog):
@@ -19,28 +22,17 @@ class TMZ(commands.Cog):
         """
         returns a location's time zone offset from UTC in minutes
         """
-        ip = str(ip)
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://api.radar.io/v1/geocode/ip', params=ip) as r:
-                expect = 'https://api.radar.io/v1/geocode/ip?ip'
-                assert str(r.url) == expect
-        print(await r.text())
+            async with session.get(
+                "https://api.radar.io/v1/geocode/ip",
+                params={"ip": ip},
+                headers={"Authorization": os.environ.get("RADAR_TOKEN")},
+            ) as r:
+                r = await r.json()
+                lat = r["address"]["latitude"]
+                long = r["address"]["longitude"]
 
-        # with open("ip.json", mode="w") as f:
-        #     json.dump(output, f, indent=4)
-
-        # with open("ip.json", mode="r") as f:
-        #     data = json.load(f)
-
-        # lat = float(data["address"]["latitude"])
-        # lng = float(data["address"]["longitude"])
-
-        # today = datetime.now()
-        # tf = TimezoneFinder()
-        # tz_target = timezone(tf.certain_timezone_at(lng=lng, lat=lat))
-        # today_target = tz_target.localize(today)
-        # today_utc = utc.localize(today)
-        # await ctx.send("You are:", (today_utc - today_target).total_seconds() / 60, "away from UTC")
+                await ctx.send(f"[{lat}, {long}]")
 
 
 def setup(bot):
