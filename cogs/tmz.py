bddb6b2a0e2c544fcf65fd050f8dafd713986249
Shaedil <shaedildider@gmail.com>
2020-08-07 23:27:33 -0400

    chore: save working directory

--- cogs/tmz.py
+++ cogs/tmz.py
@@ -5,14 +5,15 @@ from discord import Member
 
 from timezonefinder import TimezoneFinder
 from datetime import datetime
-from pytz import timezone, utc
+from pytz import timezone, utc, country_timezones
+
 
 class TMZ(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command(name="get_offset")
-    async def get_offset(self,ctx,lat,lng):
+    async def get_offset(self, ctx, lat, lng):
         """
         returns a location's time zone offset from UTC in minutes.
         """
@@ -24,8 +25,19 @@ class TMZ(commands.Cog):
         tz_target = timezone(tf.certain_timezone_at(lng=lng, lat=lat))
         today_target = tz_target.localize(today)
         today_utc = utc.localize(today)
-        await ctx.send( (today_utc - today_target).total_seconds() / 60 )
+        await ctx.send((today_utc - today_target).total_seconds() / 60)
 
+    @commands.command(name="get_")
+    async def get_(self, ctx, lat, lng):
+        """
+        returns country code
+        """
+        timezone_country = {}
+        for countrycode in country_timezones:
+            timezones = country_timezones[countrycode]
+            for timezon in timezones:
+                timezone_country[timezone] = countrycode
+                await ctx.send(countrycode)
 
 
 def setup(bot):
