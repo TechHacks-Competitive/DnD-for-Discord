efa2cd3bf302c5f62a84c30648ceea73062de7d8
Shaedil <shaedildider@gmail.com>
2020-08-08 11:50:45 -0400

    chore: wireframe of Radario api

--- cogs/tmz.py
+++ cogs/tmz.py
@@ -2,42 +2,45 @@
 import discord, time, random
 from discord.ext import commands
 from discord import Member
+import json
 
 from timezonefinder import TimezoneFinder
 from datetime import datetime
 from pytz import timezone, utc, country_timezones
+import aiohttp
 
 
 class TMZ(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="get_offset")
-    async def get_offset(self, ctx, lat, lng):
+    @commands.command(name="get_timezone")
+    async def get_timezone(self, ctx, ip):
         """
-        returns a location's time zone offset from UTC in minutes.
+        returns a location's time zone offset from UTC in minutes
         """
-        lat = float(lat)
-        lng = float(lng)
+        ip = str(ip)
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.radar.io/v1/geocode/ip', params=ip) as r:
+                expect = 'https://api.radar.io/v1/geocode/ip?ip'
+                assert str(r.url) == expect
+        print(await r.text())
 
-        today = datetime.now()
-        tf = TimezoneFinder()
-        tz_target = timezone(tf.certain_timezone_at(lng=lng, lat=lat))
-        today_target = tz_target.localize(today)
-        today_utc = utc.localize(today)
-        await ctx.send((today_utc - today_target).total_seconds() / 60)
+        # with open("ip.json", mode="w") as f:
+        #     json.dump(output, f, indent=4)
 
-    @commands.command(name="get_")
-    async def get_(self, ctx, lat, lng):
-        """
-        returns country code
-        """
-        timezone_country = {}
-        for countrycode in country_timezones:
-            timezones = country_timezones[countrycode]
-            for timezon in timezones:
-                timezone_country[timezone] = countrycode
-                await ctx.send(countrycode)
+        # with open("ip.json", mode="r") as f:
+        #     data = json.load(f)
+
+        # lat = float(data["address"]["latitude"])
+        # lng = float(data["address"]["longitude"])
+
+        # today = datetime.now()
+        # tf = TimezoneFinder()
+        # tz_target = timezone(tf.certain_timezone_at(lng=lng, lat=lat))
+        # today_target = tz_target.localize(today)
+        # today_utc = utc.localize(today)
+        # await ctx.send("You are:", (today_utc - today_target).total_seconds() / 60, "away from UTC")
 
 
 def setup(bot):
