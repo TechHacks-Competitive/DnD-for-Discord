497cf805b88a112aae3e42f9e29c903032a91b36
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-08-08 12:19:18 -0400

    loot table *nearly done

--- cogs/gen.py
+++ cogs/gen.py
@@ -8,6 +8,9 @@ with open('loot.json') as f:
     data = json.load(f)  
     
 weighted_tb = []
+def write_json(data, filename='loot.json'): 
+    with open(filename,'w') as f: 
+        json.dump(data, f, indent=4) 
 
 class Gen(commands.Cog):    
           
@@ -18,6 +21,7 @@ class Gen(commands.Cog):
     async def on_ready(self):
         print("gen.py is active")
         loot_table = data["level_1"]["magic_tb"]
+        cust_loot_table = data["level_1"]["dm_"]["discriminator"]
         for item,weight in loot_table:
             for _ in range(weight):
                 weighted_tb.append(item)
@@ -31,6 +35,28 @@ class Gen(commands.Cog):
         invn = ""
         for _ in range(items):
             invn += (random.choice(weighted_tb)) + '\n'
+        await ctx.send('```'+invn+'```')
+        
+    @commands.command()
+    async def add_loot(self, ctx, item):
+        nums = ctx.author.discriminator
+        if nums in data["level_1"]["dm_"]["discriminator"]:
+            data["level_1"]["dm_"]["discriminator"][nums].append(item)
+        else:
+            data["level_1"]["dm_"]["discriminator"][nums] = [item]
+
+        write_json(data)
+        await ctx.send("added!")
+     
+    @commands.command()
+    async def cust_loot(self, ctx, nums: Member=0):
+        if nums:
+            nums = nums.discriminator
+        else:
+            nums = ctx.author.discriminator
+        invn = ""
+        for cust_itm in data["level_1"]["dm_"]["discriminator"][nums]:
+            invn += cust_itm + '\n'
         await ctx.send('```'+invn+'```')
 
 
