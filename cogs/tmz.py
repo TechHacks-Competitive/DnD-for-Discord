d2b3ba765b0c61187d590d1d01bb2ba0e54ebc8e
Shaedil <shaedildider@gmail.com>
2020-08-08 18:27:26 -0400

    chore: add stuff

--- cogs/tmz.py
+++ cogs/tmz.py
@@ -12,44 +12,62 @@ from discord.ext import commands
 from pytz import country_timezones, timezone, utc
 from timezonefinder import TimezoneFinder
 
-
-# with open("timezone.json") as f:
-#     data = json.load(f)
-
-
-# def write_json(data, filename='timezone.json'):
-#     with open(filename, 'w') as f:
-#         json.dump(data, f, indent=4)
+bot = commands.Bot(command_prefix="$")
 
 
 class TMZ(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="nextgametime")
-    async def nextgametime(self, ctx, ip):
-        """
-        returns the next optimal time to play the next session
-        """
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                "https://api.radar.io/v1/geocode/ip",
-                params={"ip": ip},
-                headers={"Authorization": os.environ.get("RADAR_TOKEN")},
-            ) as r:
-                r = await r.json()
-                lat = r["address"]["latitude"]
-                lng = r["address"]["longitude"]
+        self.tz = {}
 
-                today = datetime.now()
-                tf = TimezoneFinder()
-                tz_target = timezone(tf.certain_timezone_at(lng=lng, lat=lat))
-                today_target = tz_target.localize(today)
-                today_utc = utc.localize(today)
-                utc_offset = (today_utc - today_target).total_seconds() / 3600
-                await ctx.send(
-                    f"You are: {utc_offset} hours away from UTC"
-                )
+        if os.path.exists("timezone.json"):
+            with open("timezone.json") as f:
+                self.tz = json.load(f)
+
+    @commands.command(name="get_timezone")
+    async def get_timezone(self, ctx, ip):
+        """
+        returns your timezone relative to UTC
+        """
+        if ctx.author.id not in self.tz:
+            # Get GMT offset and store the offset in a json object
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    "https://api.radar.io/v1/geocode/ip",
+                    params={"ip": ip},
+                    headers={"Authorization": os.environ.get("RADAR_TOKEN")},
+                ) as r:
+                    r = await r.json()
+                    lat = r["address"]["latitude"]
+                    lng = r["address"]["longitude"]
+
+                    today = datetime.now()
+                    tf = TimezoneFinder()
+                    tz_target = timezone(tf.certain_timezone_at(lng=lng, lat=lat))
+                    today_target = tz_target.localize(today)
+                    today_utc = utc.localize(today)
+                    utc_offset = (today_utc - today_target).total_seconds() / 3600
+                    await ctx.author.send(f"You are: {utc_offset} hours from UTC")
+            # Store UTC offset
+            self.tz[ctx.author.id] = []
+            self.tz[ctx.author.id].append(utc_offset)
+            with open("timezone.json", mode="w") as f:
+                json.dump(self.tz, f, indent=4)
+        elif ctx.author.id in self.tz:
+            # return the timezone from the json file
+            authortimezone = self.tz[ctx.author.id][0]
+            if authortimezone < 0:
+                await ctx.author.send(f"Your timezone: (UTC{authortimezone})")
+            elif authortimezone >= 0:
+                await ctx.author.send(f"Your timezone: (UTC+{authortimezone})")
+
+    # @commands.command(name="nextgametime")
+    # async def nextgametime(self, ctx, ip):
+    #     """
+    #     returns the best time to play the next game
+    #     """commands
+
 
 
 def setup(bot):
